b3bb0ad70cda2431417045c5595c863bb2346e6d
carlosgomezchico <145378795+carlosgomezchico@users.noreply.github.com>
2024-01-21 12:42:25 +0000

    Update DataVisualizer.java

--- src/main/java/DataVisualizer.java
+++ src/main/java/DataVisualizer.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class DataVisualizer {
+package PACKAGE_NAME;import java.util.Map;
+
+public class DataVisualizer {
+    public void displayHistogram(Map<Integer, Integer> histogram) {
+        for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
+            System.out.println("Age: " + entry.getKey() + ", Frequency: " + entry.getValue());
+        }
+    }
 }
+
