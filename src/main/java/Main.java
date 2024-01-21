0a94e63e1eb87ca68a59b73ff341345fbe66b3b9
carlosgomezchico <145378795+carlosgomezchico@users.noreply.github.com>
2024-01-21 12:42:56 +0000

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+package PACKAGE_NAME;
+public class Main {
+    public static void main(String[] args) {
+        DatasetLoader loader = new DatasetLoader();
+        List<DatasetRecord> records = loader.loadData("path/to/your/dataset.csv");
+
+        DataProcessor processor = new DataProcessor();
+        Map<Integer, Integer> histogram = processor.generateAgeHistogram(records);
+
+        DataVisualizer visualizer = new DataVisualizer();
+        visualizer.displayHistogram(histogram);
+    }
 }
